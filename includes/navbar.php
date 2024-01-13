03ff663225aab6c2861da53581cb58abf2994f10
WS350074 <145351508+WS350074@users.noreply.github.com>
2024-01-13 23:07:57 +0000

    Created navbar
    Created the base of the navbar so it can be tested and implemented in other pages.

---
+++ includes/navbar.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-md navbar-dark sticky-top bg-dark" style="background-color: #acd6de;">
+    <div class="container">
+    <a class="navbar-brand" href="#">EduTestPro</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Dashboard</a> <!-- Different for students/staff? or, only accessible for students -->
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Leaderboard</a>
+        </li>
+        <!-- Student management page -->
+        <!-- Test management page -->
+        <!-- Subject management page -->
+        <!-- Lecturer management page -->
+        <li class="nav-item">
+            <a class="nav-link" href="#">Logout</a> <!-- Logs out user -->
+        </li>     
+        </ul>
+        <span class="navbar-text nav-item">
+            <a class="nav-link" href="#">*Insert User Here*</a> <!-- Displays currently logged in user, could also take them to profile when clicked on? 
+            Could replace with student profile picture if added-->
+        </span>
+    </div>
+    </div>
+</nav>
