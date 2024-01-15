133d8431b24f752bacf99ad5c7d340ee8fe6bb99
Max Barnes <ws314697@weston.ac.uk>
2024-01-15 15:53:09 +0000

    Login Form
    I have created the login form with the input boxes in html for the username and password, also providing a login button for the user to click. I have also created a style.css file that holds all the decoration for the login form form only so far, more can be added to it for each individual page, as long as they have their own unique identifying names that will be used.

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section>
+        <form>
+            <h1>Login</h1>
+            <div class="loginInput">
+                <input type="text" required>
+                <label>Username</label>
+            </div>
+            <div class="loginInput">
+                <input type="password" required>
+                <label>Password</label>
+            </div>
+            <button>Login</button>
+            <div class="registerStudent">
+                <p>Don't have an account? 
+                    <a href="#">Register</a>
+                </p>
+            </div>
+        </form>
+    </section>
+</body>
+</html>
