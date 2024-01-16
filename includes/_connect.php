ad5cc5f526b2563d4360d297f5a5e953638ca920
Cameron <145351508+WS350074@users.noreply.github.com>
2024-01-16 13:39:05 +0000

    Created _connect.php
    Created the connect file and filled out most of the information needed to establish a database connection, I do not have access to the password on my current device and will fill that in at a later date.

---
+++ includes/_connect.php
@@ -0,0 +1,6 @@
+<?php
+$db_server = "plesk.remote.ac";
+$db_user = "ws350074_csd";
+$db_password = ""; /* Fill in later */
+$db_database = "ws350074_csd";
+$db_connect = mysqli_connect($db_server,$db_user,$db_password,$db_database);
