c9d0e6add1c4a871491e7f8372d4eb45c9ebcb63
WS313624 <lukehash714@gmail.com>
2024-01-16 14:05:42 +0000

    XAMMP Test
    Testing to see if xammp was picking up the website.

--- leaderboard.php
+++ leaderboard.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" href="#" type="image/x-icon">
 </head>
 <body>
-    
+
 </body>
 </html>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
