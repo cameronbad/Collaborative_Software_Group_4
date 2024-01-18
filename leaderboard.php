620863ccef7ce949c6798e090f74f64a8fc5fa62
WS313624 <lukehash714@gmail.com>
2024-01-18 09:46:58 +0000

    Included Navbar
    I have included the navbar onto leader board.

--- leaderboard.php
+++ leaderboard.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" href="#" type="image/x-icon">
 </head>
 <body>
-    
+    <?php include 'includes/navbar.php';?> <!-- Grabs the navbar code and displays it on the leaderboard page -->
 </body>
 </html>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
