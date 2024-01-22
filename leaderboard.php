ebc7b8aad8d9635179752ab13672adc19e55d21f
WS313624 <lukehash714@gmail.com>
2024-01-22 15:17:27 +0000

    Table styling
    I have added padding to the table centring it and I have also added spacing between the elements.

--- leaderboard.php
+++ leaderboard.php
@@ -5,15 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>leaderboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="https://cdn.datatables.net/v/bs5/dt-1.13.8/datatables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/v/bs5/dt-1.13.8/r-2.5.0/sr-1.3.0/datatables.min.css" rel="stylesheet">
     <link rel="shortcut icon" href="#" type="image/x-icon">
 </head>
-<body class="m-0 p-0">
+<body class="m-0 p-0" style="width:100%; height:100vh">
     
     <?php include 'includes/navbar.php';?> <!-- Grabs the navbar code and displays it on the leaderboard page -->
 
-    <section class="leaderboardMain">
-        <table id="leaderboard" class="leaderboard" style="width:100%">
+    <div class="container-fluid">
+        <table id="leaderboard" class="table table-primary table-hover" style="width: 80%; margin: auto; padding-top:5%; border-collapse: seperate; border-spacing:0 20px;">
             <thead>
                 <tr>
                     <th scope="col">Placement</th>
@@ -41,15 +41,27 @@
                     <td>2b</td>
                     <td>50</td>
                 </tr>
+                <tr>
+                    <th scope="row">4</th>
+                    <td>Luke</td>
+                    <td>2b</td>
+                    <td>50</td>
+                </tr>
+                <tr>
+                    <th scope="row">5</th>
+                    <td>Luke</td>
+                    <td>2b</td>
+                    <td>50</td>
+                </tr>
             </tbody>
         </table>
-    </section>
+</div>
 
 </body>
 </html>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/v/bs5/dt-1.13.8/datatables.min.js"></script>
+<script src="https://cdn.datatables.net/v/bs5/dt-1.13.8/r-2.5.0/sr-1.3.0/datatables.min.js"></script>
 <script> new DataTable('#leaderboard',{
     paging: false,
     ordering: false,
