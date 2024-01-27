2cb412faf77201b33689747d877b6f80d1cd5726
WS350074 <145351508+WS350074@users.noreply.github.com>
2024-01-27 16:54:40 +0000

    Test creation functionality
    Functionality for creating tests implemented successfully.

---
+++ createTest.php
@@ -0,0 +1,31 @@
+<?php
+
+
+if (!isset($_POST['tSubjectSelect']) ||
+    !isset($_POST['tName']) ||
+    !isset($_POST['tAmount'])
+) {
+    die("Please fill out all fields");
+}
+
+require_once("includes/_connect.php");
+
+//Declare php variable's from post, creates a legal SQL string to avoid issues.
+$subject = mysqli_real_escape_string($db_connect, $_POST['tSubjectSelect']);
+$name = mysqli_real_escape_string($db_connect, $_POST['tName']);
+$amount = mysqli_real_escape_string($db_connect, $_POST['tAmount']);
+
+echo "Test";
+
+//Prepare SQL query
+$query = "INSERT INTO `test` (`testID`, `subjectID`, `testName`, `questionAmount`) VALUES (NULL, ?, ?, ?)";
+$stmt = mysqli_prepare($db_connect, $query);
+
+//Bind parameters
+mysqli_stmt_bind_param($stmt, "isi", $subject, $name, $amount);
+
+if (mysqli_stmt_execute($stmt))
+    echo "Test created succesfully";
+else
+    echo "Error: " . mysqli_error($db_connect);
+?>
--- testManagement.php
+++ testManagement.php
@@ -160,8 +160,24 @@
         <!-- END EDIT MODAL -->
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script>
-        const testModal = document.getElementByID('testModal');
+        //AJAX call to create a test
+        $('#testForm').submit(function (e) {
+            e.preventDefault();
+            $.ajax({
+                url: "./createTest.php",
+                method: "POST",
+                data: $('#testForm').serialize(),
+                success: function(data) {
+                    alert(data);
+                    //location.reload();
+                }
+            })
+        });
+
+        //Modal JS
+        const testModal = document.getElementById('testModal');
         testModal.addEventListener('show.bs.modal', event => {
 
         //var courseSelect = document.getElementByID('courseSelect');
@@ -174,7 +190,7 @@
         const testID = button.getAttribute('data-bs-tid');
 
         //Update content
-        const eTestID = document.getElementByID("eTestID");
+        const eTestID = document.getElementById("eTestID");
         eTestID.value = testID;
 
         });
