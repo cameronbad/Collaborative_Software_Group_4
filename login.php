4f1c2e0a16edbf9bd12c4e71865b7ac4e39e1592
Max Barnes <ws314697@weston.ac.uk>
2024-01-29 14:51:21 +0000

    Adding logo icon to tab
    I have added the line of code needed to display the logo for the project within the tab of the browser window that will ensure conformity with the other pages of the project.

--- login.php
+++ login.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="shortcut icon" href="Images/EduTestLogo.png" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
