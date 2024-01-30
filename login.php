9f4c865bc80d540260abf3e23ab1a029cde77312
Max Barnes <ws314697@weston.ac.uk>
2024-01-30 13:52:47 +0000

    Class names
    Adding the class names for the login title.

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
         <!--CREATES FORM AREA -->
         <form class="loginForm" action="intoDashboard.php" method="post">
             <img class="loginLogo" src="./Images/EduTestLogo.png" alt="logo" style="max-width:150px;height:150px;margin-left:18%;">
-            <h1>Login</h1>
+            <h1 class="loginTitle">Login</h1>
             <!--CREATES ERROR MESSAGE -->
             <?php if (isset($_GET['error'])) { ?>
                 <p class="error">
