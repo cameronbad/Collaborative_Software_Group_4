4950e9151243a29ed1545accc79e44c2332355bb
Ashen <aihbef@gmail.com>
2024-01-30 14:05:14 +0000

    Update subjectManagement.php

--- subjectManagement.php
+++ subjectManagement.php
@@ -11,52 +11,25 @@
     <link rel="shortcut icon" href="Images/EduTestLogo.png" type="image/x-icon">
 </head>
   <body>
+  <?php include_once("includes/navbar.php"); ?>
     <h1>subject management page</h1>
 
-    <div class="modal" tabindex="-1">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>Modal body text goes here.</p>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <p>test to see if scroll is auto implemented</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </div>
+    <table id="modalTable" class="table table-bordered table-striped table-card">
+
+       <thead>
+          <tr>
+             <th>ID</th>
+                <th>Subject Name</th>
+                <!--<?php if ($_SESSION['accessLevel'] == '3') {echo '<th>Course</th>';}?> -->
+                <th>Number of Questions</th>
+                <th>Questions</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+    
   </body>
 </html>
