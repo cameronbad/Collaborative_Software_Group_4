6f56bcbdde89922cc508776bc3d7c3a46efcc3e9
Max Barnes <ws314697@weston.ac.uk>
2024-02-01 10:00:42 +0000

    Registration inputs
    I have added the code that creates the inputs that users need to fill in on the page

--- registration.php
+++ registration.php
@@ -16,6 +16,41 @@
             <img class="registerLogo" src="./Images/EduTestLogo.png" alt="logo" style="max-width:150px;height:150px;margin-left:18%;">
             <h1 class="registerTitle">Register</h1>
             <!--CREATES INPUTS FOR USER DATA -->
+            <div class="registerInput">
+                <label>First Name</label>
+                <input type="text" id="firstName" name="firstName" placeholder="Enter first name...">
+            </div>
+            <div class="registerInput">
+                <label>Last Name</label>
+                <input type="text" id="lastName" name="lastName" placeholder="Enter last name...">
+            </div>
+            <div class="registerInput">
+                <label>Email</label>
+                <input type="email" id="emailInput" name="emailInput" placeholder="Enter email...">
+            </div>
+            <div class="registerInput">
+                <label>Confirm Email</label>
+                <input type="email" id="confirmEmailInput" name="confirmEmailInput" placeholder="Confirm email...">
+            </div>
+            <div class="registerInput">
+                <label>Username</label>
+                <input type="text" id="userInput" name="userInput" placeholder="Enter username...">
+            </div>
+            <div class="registerInput">
+                <label>Password</label>
+                <input type="password" id="passInput" name="passInput" placeholder="Enter password...">
+            </div>
+            <div class="registerInput">
+                <label>Confirm Password</label>
+                <input type="password" id="confirmPassInput" name="confirmPassInput" placeholder="Confirm password...">
+            </div>
+            <button class="registerButton" type="submit">Register</button>
+            <!--CREATES LINK TO LOGIN PAGE -->
+            <div class="loginStudent">
+                <p>Already have an account?
+                    <a href="login.php">Login</a>
+                </p>
+            </div>
         </form>
     </section>
 </body>
