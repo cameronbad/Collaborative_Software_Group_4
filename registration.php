63728b0dfe5d09506b340cf61ac2fe17a7221284
Max Barnes <ws314697@weston.ac.uk>
2024-02-01 10:45:25 +0000

    Page title
    Have changed the page title from Login to registration so the tab of the page displays correctly

--- registration.php
+++ registration.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Registration</title>
     <link rel="shortcut icon" href="Images/EduTestLogo.png" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
