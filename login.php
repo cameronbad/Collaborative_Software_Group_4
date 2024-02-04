600db51139299e65b251972317c62fd61e37f21a
Max Barnes <ws314697@weston.ac.uk>
2024-02-04 11:45:28 +0000

    Removing inline css
    Following @WS350074 comment about inline css, i have removed that and added the needed alterations to the class css code to ensure that the logo looked the same as before.

--- login.php
+++ login.php
@@ -13,7 +13,7 @@
     <section class="loginSection">
         <!--CREATES FORM AREA -->
         <form class="loginForm" action="intoDashboard.php" method="post">
-            <img class="loginLogo" src="./Images/EduTestLogo.png" alt="logo" style="max-width:150px;height:150px;margin-left:18%;">
+            <img class="loginLogo" src="./Images/EduTestLogo.png" alt="logo">
             <h1 class="loginTitle">Login</h1>
             <!--CREATES ERROR MESSAGE -->
             <?php if (isset($_GET['error'])) { ?>
