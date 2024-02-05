cd4105c24a466435490b1b4a5ae2f5c4c2688b10
Ashen <aihbef@gmail.com>
2024-02-05 03:03:39 +0000

    didn't get modals working sorting it out at uni tomorrow

--- subjectManagement.php
+++ subjectManagement.php
@@ -11,66 +11,21 @@
     <link rel="shortcut icon" href="Images/EduTestLogo.png" type="image/x-icon">
 </head>
   <body>
-    <h1>subject management page</h1>
-<<<<<<< Updated upstream
-
-    <div class="modal" tabindex="-1">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>Modal body text goes here.</p>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <p>test to see if scroll is auto implemented</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </div>
-=======
+    <h1 class:"arTitle">subject management page</h1>
         <thead>
                     <tr>
                         <th>ID</th>
                         <th>Subject Name</th>
-                        <!--<?php if ($_SESSION['accessLevel'] == '3') {echo '<th>Course</th>';}?>-->
                         <th>Assigned Tests</th>
                         <th>Edit</th>
                         <th>Delete</th>
                     </tr>
                 </thead>
-    
->>>>>>> Stashed changes
+    <tbody>
+        <?php
+            include_once("includes/_connect.php");
+            //i didn't manage to get anything going unfortunately 
+        ?>
+    </tbody>
   </body>
 </html>
