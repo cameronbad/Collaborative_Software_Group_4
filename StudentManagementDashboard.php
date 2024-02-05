8c89b5ec718df883473d858c8010d5e3dc5406ba
WS313624 <lukehash714@gmail.com>
2024-02-05 14:45:20 +0000

    Student Managment Table Creation
    I have created the table and given it some styling.

--- StudentManagementDashboard.php
+++ StudentManagementDashboard.php
@@ -7,8 +7,39 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="shortcut icon" href="#" type="image/x-icon">
 </head>
-<body>
-    
+<body id="studentBody" class="m-0 p-0">
+
+<?php include('includes/navbar.php');?> <!-- Grabs the navbar code and displays it on the student management page -->
+
+<div class="container">
+    <div class="container mt-5" id="studentTableMain">
+        <table class="table table-hover table-striped table-bordered">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                   <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+               </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
 </body>
 </html>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
