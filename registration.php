e8cd733405bf96d84613143c8e691c1da3653520
Max Barnes <ws314697@weston.ac.uk>
2024-02-05 15:22:59 +0000

    Bootstrap Testing
    I have started playing around with bootstrap form grids as a method of making the registration form more appealing instead of one long form with the inputs in a list like format.

--- registration.php
+++ registration.php
@@ -7,6 +7,8 @@
     <title>Registration</title>
     <link rel="shortcut icon" href="Images/EduTestLogo.png" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body class="registerBody">
@@ -53,6 +55,45 @@
                     <option value="4">Computer Engineering</option>
                 </select>
             </div>
+
+            <!-- <div class="row">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Student Number" aria-label="Student Number">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="First Name" aria-label="First name">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Last Name" aria-label="Last Name">
+                </div>
+                <div class="col">
+                    <input type="email" class="form-control" placeholder="Email" aria-label="Email">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Username" aria-label="Username">
+                </div>
+                <div class="col">
+                    <input type="password" class="form-control" placeholder="Password" aria-label="Password">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <input type="password" class="form-control" placeholder="Confirm Password" aria-label="Confirm Password">
+                </div>
+                <div class="col">
+                    <select class="form-select" aria-label="Course">
+                        <option selected>Course</option>
+                        <option value="1">Computer Science</option>
+                        <option value="2">Information Technology</option>
+                        <option value="3">Software Engineering</option>
+                        <option value="4">Computer Engineering</option>
+                    </select>
+                </div>
+            </div> -->
             <button class="registerButton" type="submit">Register</button>
             <!--CREATES LINK TO LOGIN PAGE -->
             <div class="loginStudent">
@@ -62,6 +103,9 @@
             </div>
         </form>
     </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
