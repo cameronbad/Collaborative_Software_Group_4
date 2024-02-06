1144e7bf3e4f7487bc31370825f2bf61f349f717
WS313624 <lukehash714@gmail.com>
2024-02-06 14:08:04 +0000

    Leaderboard Comment and Cleanup
    I have modelized the code further by putting the sql into a separate file to be called while adding additional comments.

--- leaderboard.php
+++ leaderboard.php
@@ -20,34 +20,7 @@
         <form class="row" method="POST" id="filterBox">
             <div class="col-10">
                 <select class="form-select col" id="classFilters" name="classFilters">
-                    <?php
-                        require_once ('includes/_connect.php');
-
-                        $SQL = "CALL allCourseNames()";// Calls the procedure
-
-                        $result = mysqli_query($db_connect, $SQL);
-
-                        $preValue = '0';
-
-                        while(mysqli_next_result($db_connect)){;} //Fixes Unsynch Error
-
-                        while($row = mysqli_fetch_assoc($result)){ //Loops through results and matches the subjects with the courses
-                            if (/*$_SESSION['courseID'] Add this once session values*/1 == $row['courseID'] || $_SESSION['accessLevel'] == '3') {
-                                if($preValue == 0) {
-                                    echo "<optgroup label=" . $row["courseName"] . ">";
-                                    echo "<option value='" . $row["subjectID"] . "'>" . $row["subjectName"] . "</option>";
-                                    $preValue = $row["courseID"];
-                                } else if ($preValue != $row["courseID"]) {
-                                    echo "</optgroup>";
-                                    echo "<optgroup label=" . $row["courseName"] . ">";
-                                    echo "<option value='" . $row["subjectID"] . "'>" . $row["subjectName"] . "</option>";
-                                    $preValue = $row["courseID"];
-                                } else {
-                                    echo "<option value='" . $row["subjectID"] . "'>" . $row["subjectName"] . "</option>";
-                                }
-                            }
-                        }
-                    ?>
+                    <?php include_once("includes/filtersDisplay.php") ?> <!-- Displays subjects for filters -->
                 </select>
             </div>
             <div class="col-2">
@@ -66,7 +39,7 @@
                 </tr>
             </thead>
             <tbody id="leaderboardDisplay"><!-- Table Contents -->
-                        <?php  include_once("includes/defaultScorers.php"); ?>
+                        <?php  include_once("includes/defaultScorers.php"); ?> <!-- Grabs a default table based on the users course -->
             </tbody>
         </table>
     </div>
