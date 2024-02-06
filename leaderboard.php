520cc4fa6945b3f8f0f20d7e9944c257350e8d33
WS313624 <lukehash714@gmail.com>
2024-02-06 14:00:22 +0000

    Leaderboard Filter Access
    I have now added limited user access based on the course you are in as it will now only allows users to view subjects in their own course except admins can view everything.

--- leaderboard.php
+++ leaderboard.php
@@ -32,17 +32,19 @@
                         while(mysqli_next_result($db_connect)){;} //Fixes Unsynch Error
 
                         while($row = mysqli_fetch_assoc($result)){ //Loops through results and matches the subjects with the courses
-                            if($preValue == 0) {
-                                echo "<optgroup label=" . $row["courseName"] . ">";
-                                echo "<option value='" . $row["subjectID"] . "'>" . $row["subjectName"] . "</option>";
-                                $preValue = $row["courseID"];
-                            } else if ($preValue != $row["courseID"]) {
-                                echo "</optgroup>";
-                                echo "<optgroup label=" . $row["courseName"] . ">";
-                                echo "<option value='" . $row["subjectID"] . "'>" . $row["subjectName"] . "</option>";
-                                $preValue = $row["courseID"];
-                            } else {
-                                echo "<option value='" . $row["subjectID"] . "'>" . $row["subjectName"] . "</option>";
+                            if (/*$_SESSION['courseID'] Add this once session values*/1 == $row['courseID'] || $_SESSION['accessLevel'] == '3') {
+                                if($preValue == 0) {
+                                    echo "<optgroup label=" . $row["courseName"] . ">";
+                                    echo "<option value='" . $row["subjectID"] . "'>" . $row["subjectName"] . "</option>";
+                                    $preValue = $row["courseID"];
+                                } else if ($preValue != $row["courseID"]) {
+                                    echo "</optgroup>";
+                                    echo "<optgroup label=" . $row["courseName"] . ">";
+                                    echo "<option value='" . $row["subjectID"] . "'>" . $row["subjectName"] . "</option>";
+                                    $preValue = $row["courseID"];
+                                } else {
+                                    echo "<option value='" . $row["subjectID"] . "'>" . $row["subjectName"] . "</option>";
+                                }
                             }
                         }
                     ?>
