89c152243af96d66b8d1903fae52d67be19d0331
Max Barnes <ws314697@weston.ac.uk>
2024-02-22 09:53:40 +0000

    Unneeded Code
    I have removed the commented code that I was using for the registration form before I implemented bootstrap. This will now reduce the clutter in both the registration.php file and the stylesheet.

--- registration.php
+++ registration.php
@@ -18,44 +18,6 @@
             <img class="registerLogo" src="./Images/EduTestLogo.png" alt="logo">
             <h1 class="registerTitle">Register</h1>
             <!--CREATES INPUTS FOR USER DATA -->
-            <!-- <div class="registerInput">
-                <label id="reglabel">Student Number</label>
-                <input type="text" id="studentID" name="studentNum" placeholder="Enter student number...">
-            </div>
-            <div class="registerInput">
-                <label id="registerLabel">First Name</label>
-                <input type="text" id="firstName" name="firstName" placeholder="Enter first name...">
-            </div>
-            <div class="registerInput">
-                <label id="registerLabel">Last Name</label>
-                <input type="text" id="lastName" name="lastName" placeholder="Enter last name...">
-            </div>
-            <div class="registerInput">
-                <label>Email</label>
-                <input type="email" id="emailInput" name="emailInput" placeholder="Enter email...">
-            </div>
-            <div class="registerInput">
-                <label id="registerLabel">Username</label>
-                <input type="text" id="userInput" name="userInput" placeholder="Enter username...">
-            </div>
-            <div class="registerInput">
-                <label id="registerLabel">Password</label>
-                <input type="password" id="passInput" name="passInput" placeholder="Enter password...">
-            </div>
-            <div class="registerInput">
-                <label id="registrationLabel">Confirm Password</label>
-                <input type="password" id="confirmPassInput" name="confirmPassInput" placeholder="Confirm password...">
-            </div>
-            <div class="registerInput">
-                <label>Course</label>
-                <select id="courseInput" name="courseInput">
-                    <option value="1">Computer Science</option>
-                    <option value="2">Information Technology</option>
-                    <option value="3">Software Engineering</option>
-                    <option value="4">Computer Engineering</option>
-                </select>
-            </div> -->
-
             <div class="row mb-3">
                 <div class="col">
                     <input type="text" class="form-control" placeholder="Student Number" id="studentNum"
