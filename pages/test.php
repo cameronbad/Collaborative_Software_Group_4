22beea9b3818e7c3c444114b7f9f365d5fadf2a0
WS350074 <145351508+WS350074@users.noreply.github.com>
2024-02-23 20:07:15 +0000

    File Structure & test-bottom
    Moved test.php to pages folder and added route in index.php, did functionality work on the bottom bar of the page and made styling using <svg> to.

--- pages/test.php
+++ pages/test.php
@@ -1,14 +1,21 @@
 <?php
 //User auth here / Only user doing the test should be able to access the page.
-session_start();
+@session_start();
 
+//need to $_GET the result ID
+
+require_once("./includes/_connect.php");
+
+$query = "SELECT `test`.`testName`, `subject`.`subjectName`, `result`.`questionTotal`, `result`.`questionCurrent` FROM `result` LEFT JOIN `test` ON `result`.`testID` = `test`.`testID` LEFT JOIN `subject` ON `test`.`subjectID` = `subject`.`subjectID` WHERE `result`.`resultID` = '13'"; //Replace 13 with a GET['resultID'] 
+$test = mysqli_fetch_assoc(mysqli_execute_query($db_connect, $query));
+$current = $test['questionCurrent'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EduTest | Test <!-- ID or name of test here? --> </title>
+    <title>EduTest | Test | <?= $test['testName'] ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="style.css" rel="stylesheet">
     <link rel="shortcut icon" href="Images/EduTestLogo.png" type="image/x-icon">
@@ -21,13 +28,20 @@ session_start();
             <!-- Question / Answers / Submit || Add each question to this container / Existing answers get added on load (initial one created  on first load?)
             Later answers get added by ajax query using page template? -->
         </div>  
-        <div class="navbar navbar-dark fixed-bottom bg-dark">
-            <div class="navbar-text">
-                <!-- Subject | Test name //Could do a colour change between the subject text and the test name //Use vector graphics for a diagonal line?-->
+        <div class="navbar fixed-bottom test-bottom">
+            <div class="navbar-nav flex-row">
+                <div class="navbar-text p-3"><?= $test['subjectName'] ?></div>
+                <svg class="test-bottom-svg d-inline-block align-text-top">
+                    <polygon points='0,0 0,56 30,0'>
+                </svg>  
+                <div class="navbar-text p-3"><?= $test['testName'] ?></div>
             </div>
-            <div class="navbar-text">
-                <!-- Current of total eg Question 1 of 5, 2 of 5 -->
-            </div>     
+            <div class="navbar-nav flex-row">
+                <svg class="test-bottom-svg d-inline-block align-text-top">
+                    <polygon points='30,56 0,56 30,0'>
+                </svg>  
+                <div class="navbar-text p-3"><?= $current ?> of <?= $test['questionTotal'] ?></div>
+            </div>    
         </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
