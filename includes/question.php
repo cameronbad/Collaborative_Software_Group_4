2099eeea3f6a90a6404cd67d6eafc809355203ac
WS350074 <145351508+WS350074@users.noreply.github.com>
2024-02-24 15:50:33 +0000

    Question populate
    Questions are populated with question text and answers, the order of answers is successfully randomised.

--- includes/question.php
+++ includes/question.php
@@ -1,13 +1,29 @@
-<div class="container">
-    <div class="row">
+<?php
+include_once("includes/_connect.php");  
 
+$_GET['questionID'] = '1'; //temp
+
+$query = "SELECT `question`.`questionText`, `question`.`answerA`, `question`.`answerB`, `question`.`answerC`, `question`.`answerD` FROM `question` WHERE `question`.`questionID` = " . $_GET['questionID'];
+$question = $db_connect->execute_query($query)->fetch_assoc();
+
+//Make array with answers assigned to value 1-4
+$answers = array('1'=>$question['answerA'],'2'=>$question['answerB'],'3'=>$question['answerC'],'4'=>$question['answerD']);
+
+//Shuffle the value/keys
+$keys = array_keys($answers);
+shuffle($keys);
+
+?>
+<div class="container position-absolute top-50 start-50 translate-middle card"> <!-- card is temp for styling-->
+    <div class="row">
+        <h3 class="text-center"><?= $question['questionText'] ?></h3>      
     </div>
     <div class="row">
-        <div class="col"></div>
-        <div class="col"></div>
+        <div class="col"><?= $answers[$keys[0]] ?></div>
+        <div class="col"><?= $answers[$keys[1]] ?></div>
     </div>
     <div class="row">
-        <div class="col"></div>
-        <div class="col"></div>
+        <div class="col"><?= $answers[$keys[2]] ?></div>
+        <div class="col"><?= $answers[$keys[3]] ?></div>
     </div>
 </div>
