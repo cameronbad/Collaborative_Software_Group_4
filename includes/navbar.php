06aa059ac64360ae699e89e4d7d7790282925604
WS350074 <145351508+WS350074@users.noreply.github.com>
2024-02-24 14:40:09 +0000

    Updated navbar routing
    Added functional page routing to the navbar and made links appear based off of user accessLevel.

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-dark sticky-top bg-dark" style="background-color: #acd6de;">
+<nav class="navbar navbar-expand-md sticky-top navbar-dark bg-dark">
     <div class="container">
     <a class="navbar-brand" href="#">EduTestPro</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -6,22 +6,41 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
+        <?php 
+        if($_SESSION['accessLevel'] == '1') {
+            echo '<li class="nav-item">
+                <a class="nav-link" href="./testDashboard">Dashboard</a>
+            </li>';
+        }
+        
+        if($_SESSION['accessLevel'] == '2' || $_SESSION['accessLevel'] == '3') {
+            echo '<li class="nav-item">
+                <a class="nav-link" href="./studentDashboard">Students</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./testManagement">Tests</a>
+            </li>';
+        }
+        if($_SESSION['accessLevel'] == '3') {
+            echo '<li class="nav-item">
+                <a class="nav-link" href="#">Lecturers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Subjects</a>
+            </li>';
+        }   
+        ?>
+        
+
         <li class="nav-item">
-            <a class="nav-link" href="#">Dashboard</a> <!-- Different for students/staff? or, only accessible for students -->
+            <a class="nav-link" href="./leaderboard">Leaderboard</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Leaderboard</a>
-        </li>
-        <!-- Student management page -->
-        <!-- Test management page -->
-        <!-- Subject management page -->
-        <!-- Lecturer management page -->
-        <li class="nav-item">
-            <a class="nav-link" href="#">Logout</a> <!-- Logs out user -->
+            <a class="nav-link" href="./functionality/logout.php">Logout</a> <!-- Logs out user -->
         </li>     
         </ul>
         <span class="navbar-text nav-item">
-            <a class="nav-link" href="#">*Insert User Here*</a> <!-- Displays currently logged in user, could also take them to profile when clicked on? 
+            <a class="nav-link" href="#"><?= $_SESSION['username'] ?></a> <!-- Displays currently logged in user, could also take them to profile when clicked on? 
             Could replace with student profile picture if added-->
         </span>
     </div>
