b3a33b68384bc8bab5904f335a56c35b70dacc46
WS350074 <145351508+WS350074@users.noreply.github.com>
2024-02-24 00:58:12 +0000

    testManagement Table Enhancement
    Moved the table on testManagement.php to a separate page so that I can use ajax to have the table reload whenever an ajax query is run, this will act as the base for any error handling I implement later.

--- pages/testManagement.php
+++ pages/testManagement.php
@@ -19,45 +19,8 @@
         <?php include_once("includes/navbar.php"); ?>
         <div class="container mt-5">
             <!-- Table to display list of tests to manage -->
-            <table id="modalTable" class="table table-bordered table-striped table-card">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Test Name</th>
-                        <?php if ($_SESSION['accessLevel'] == '3') {echo '<th>Course</th>';}?> 
-                        <th>Subject</th>
-                        <th>Amount of Questions</th>
-                        <th>Assign</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    include_once("includes/_connect.php");
-
-                    //Get query for table
-                    if ($_SESSION['accessLevel'] == '3') { //If admin account
-                        $query = "SELECT `test`.*, `subject`.`subjectName`, `subject`.`courseID`, `course`.`courseName` FROM `test` LEFT JOIN `subject` ON `test`.`subjectID` = `subject`.`subjectID` LEFT JOIN `course` ON `subject`.`courseID` = `course`.`courseID`" ; 
-                    } else { //If teacher account
-                        $query = "SELECT `test`.*, `subject`.`subjectName`, `subject`.`courseID` FROM `test` LEFT JOIN `subject` ON `test`.`subjectID` = `subject`.`subjectID` WHERE `subject`.`courseID` = 1" ; //. $_SESSION["courseID"]
-                    }
-
-                    $run = $db_connect->query($query);
-                    while ($result = $run->fetch_assoc()) {
-                        echo "<tr>";
-                        echo "<td>" . $result["testID"] . "</td>";
-                        echo "<td>" . $result["testName"] . "</td>";
-                        if ($_SESSION['accessLevel'] == '3') {echo "<td>" . $result["courseName"] . "</td>";}
-                        echo "<td>" . $result["subjectName"] . "</td>";
-                        echo "<td>" . $result["questionAmount"] . "</td>"; 
-                        echo "<td> <button type='button' class='btn btn-success' data-bs-tid='" . $result["testID"] . "' data-bs-cid='" . $result["courseID"] . "'data-bs-toggle='modal' data-bs-target='#assignModal'>Assign</button></td>";
-                        echo "<td> <button type='button' class='btn btn-primary' data-bs-tid='" . $result["testID"] . "'data-bs-toggle='modal' data-bs-target='#editModal'>Edit</button></td>";
-                        echo "<td> <button type='button' class='btn btn-danger'  data-bs-tid='" . $result["testID"] . "'data-bs-toggle='modal' data-bs-target='#deleteModal'>Remove</button> </td>";
-                        echo "</tr>";
-                    }
-                    ?>
-                </tbody>
+            <table id="testManagementTable" class="table table-bordered table-striped table-card">
+                <?php include("includes/testManagementTable.php"); ?>
             </table>
 
             <!-- Button to create new tests -->
@@ -266,7 +229,7 @@
                 method: "POST",
                 data: $('#testForm').serialize(),
                 success: function(data) {
-                    location.reload();
+                    //alert(data);
                 }
             })
         });
@@ -279,7 +242,7 @@
                 method: "POST",
                 data: $('#editForm').serialize(),
                 success: function(data) {
-                    location.reload();
+                    //alert(data);
                 }
             })
         });
@@ -305,9 +268,20 @@
                 method: "POST",
                 data: $('#deleteForm').serialize(),
                 success: function(data) {
-                    location.reload();
+                    //alert(data);
                 }
             })
+        });
+        
+        //Updates test whenever an AJAX call is complete
+        $(document).on("ajaxSuccess", function() {
+           $.ajax({
+                url: './includes/testManagementTable.php',
+                global: false,
+                success: function(data) {
+                    $('#testManagementTable').html(data);
+                }
+           }); 
         });
 
         //Edit Modal JS
@@ -343,6 +317,7 @@
             $.ajax({
                 url: "./includes/assignModal.php",
                 method: "POST",
+                global: false,
                 data: {courseID:courseID},
                 success: function(data)
                 {
