fbd48783f91b9137815c05a49f0579d2dd691cc9
WS313624 <lukehash714@gmail.com>
2024-02-24 18:26:57 +0000

    Student Profile Form Update
    I have now added the correct number of rows and changed the labels to correspond. Additionally I have created a routine for displaying the data.

--- pages/studentProfile.php
+++ pages/studentProfile.php
@@ -27,22 +27,46 @@
             </div>
         </div>
             <div class="studentProfileRight">
+
+            <?php
+                $SQL="CALL studentPRofileUser(1)";
+            ?>
+
             <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+                <div class="mb-3">
+                    <label for="studentProfileID" class="form-label">ID</label>
+                    <input type="text" class="form-control" id="studentProfileID">
+                </div>
+                <div class="mb-3">
+                    <label for="studentProfileUsername" class="form-label">Username</label>
+                    <input type="text" class="form-control" id="studentProfileUsername">
+                </div>
+                <div class="mb-3">
+                    <label for="studentProfileName" class="form-label">Name</label>
+                    <input type="text" class="form-control" id="studentProfileName">
+                </div>
+                <div class="mb-3">
+                    <label for="studentProfileEmail" class="form-label">Email</label>
+                    <input type="text" class="form-control" id="studentProfileEmail">
+                </div>
+                <div class="mb-3">
+                    <label for="studentProfileNumber" class="form-label">Student Number</label>
+                    <input type="text" class="form-control" id="studentProfileNumber">
+                </div>
+                <div class="mb-3">
+                    <label for="studentProfileCourse" class="form-label">Course</label>
+                    <input type="text" class="form-control" id="studentProfileCourse">
+                </div>
+                <div class="mb-3">
+                    <label for="studentProfileState" class="form-label">Account State</label>
+                    <input type="text" class="form-control" id="studentProfileState">
+                </div>
+                <div class="mb-3">
+                    <label for="studentProfileLogin" class="form-label">Last Login</label>
+                    <input type="text" class="form-control" id="studentProfileLogin">
+                </div>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
             </div>
         <div>
         </div>
