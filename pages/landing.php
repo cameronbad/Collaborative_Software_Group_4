fa5b5ddfd8cccf4b5b78ecf7b1200b50d5ea7f38
Max Barnes <ws314697@weston.ac.uk>
2024-02-26 15:51:32 +0000

    Landing Development
    I have no created the main body of the landing page with animation and the correct colours chosen that match the colours in the housestyle sheet.

--- pages/landing.php
+++ pages/landing.php
@@ -1,13 +1,68 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Landing</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>EduTestPro</title>
+    <!-- Boostrap CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Box Icons -->
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
     <link rel="shortcut icon" href="Images/EduTestLogo.png" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- Animate JS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
 </head>
-<body class="landingBody">
-    
+
+<body>
+
+    <div class="container-fluid">
+        <div class="row no-gutter">
+            <!-- The image half -->
+            <div class="col-md-6 d-none d-md-flex bg-image animate__animated animate__fadeInUp"></div>
+            <!-- The content half -->
+            <div class="col-lg-6 col-md-12 col-sm-12">
+                <div class="content d-flex align-items-center">
+
+                    <!-- Demo content-->
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-10 col-xl-11 mx-auto">
+                                <div class="text__area">
+                                    <div class="landingLogo">
+                                        <img class="landingLogo" src="./Images/EduTestLogo.png" alt="logo">
+                                    </div>
+                                    <div class="flex align label animate__animated animate__bounceInUp pt-2">
+                                        <i class='bx bxs-arrow-to-right py-1'></i>
+                                        <p class="ms-2">An exciting way to learn</p>
+                                    </div>
+                                    <div class="text">
+                                        <h5 class="display-5 animate__animated animate__bounceInUp">Use a new way of test taking to improve your test results.
+                                        </h5>
+                                        <p class="animate__animated animate__bounceInUp">Hello</p>
+                                    </div>
+                                    <a href="/login" class="btn btn-primary animate__animated animate__bounceInUp">Login <i class='bx bx-right-arrow-alt'></i></a>
+                                    <a href="/registration" class="btn btn-primary animate__animated animate__bounceInUp">Register <i class='bx bx-right-arrow-alt'></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- End -->
+
+                </div>
+            </div><!-- End -->
+
+
+        </div>
+    </div>
+
+    <!-- Boostrap JS CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
