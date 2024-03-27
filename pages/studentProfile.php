64762a6e9e804d2cbc011dc12da32e4ee255077b
WS313624 <lukehash714@gmail.com>
2024-03-27 15:11:04 +0000

    Authentication
    The pages I have created leaderboard, student management and class management all now redirect users if they are not logged in or have the correct permissions.

--- pages/studentProfile.php
+++ pages/studentProfile.php
@@ -1,3 +1,6 @@
+<?php include_once("functionality/loginCheck.php") ?> <!-- Checks if the user has logged in -->
+<?php include_once("functionality/authCheck.php") ?> <!-- Checks if the user is of the correct access level -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
